cab5803ede73f33f5b4192f4c61f57292a5e31de
fbotu <francisbotu@gmail.com>
2022-01-04 04:50:35 +0000

    Routes added to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import Navbar from './components/Navbar/Navbar';
+import { Routes, Route, Link } from 'react-router-dom'
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Footer from './components/Footer';
+
 
 function App() {
     return (
         <div className='App'>
             <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/" element={<About />} />
+            </Routes>
+            <Footer />
         </div>
     )
 }
